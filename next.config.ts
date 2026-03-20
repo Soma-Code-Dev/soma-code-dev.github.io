df218625d1809573a0e41d0d1a9a716e883bdb60
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-03-20 13:17:39 +0900

    build: Configure Next.js for static export and disable image optimization.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
